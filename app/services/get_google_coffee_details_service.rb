b329143af97c9eaf1dc5351c2a6b1c5560f9d2b2
Stálin Liberato <stalinlbessa@gmail.com>
2021-01-29 07:58:00 -0400

    method call coffeelist

--- app/services/get_google_coffee_details_service.rb
+++ app/services/get_google_coffee_details_service.rb
@@ -4,19 +4,10 @@ require 'json'
 
 class GetGoogleCoffeeDetailsService
     def initialize(latitude, longitude)
-        @latitude = latitude
-        @longitude = longitude
+       
     end
 
     def call()
-        begin
-            key = #{Rails.application.credentials.google_secret_key}
-            location = "#{latitude}, #{longitude}"
-            radius = "5000"
-            base url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=coffee+shops&#{location}&#{radius}&#{key}"
-            response = RestClient.get base_url
-            JSON.parse(response.body)
-        rescue RestClient::ExceptionWithResponde =>
-            e.response
+       
     end
 end
--- app/services/get_google_coffee_list_service.rb
+++ app/services/get_google_coffee_list_service.rb
@@ -2,10 +2,20 @@ require 'rest-client'
 require 'json'
 
 class GetGoogleCoffeeListService
-    def initialize()
+    def initialize(latitude, longitude)
+        @latitude = latitude
+        @longitude = longitude
     end
 
     def call()
-
+        begin
+            key = #{Rails.application.credentials.google_secret_key}
+            location = "#{latitude}, #{longitude}"
+            radius = "5000"
+            base url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=coffee+shops&#{location}&#{radius}&#{key}"
+            response = RestClient.get base_url
+            JSON.parse(response.body)
+        rescue RestClient::ExceptionWithResponde =>
+            e.response
     end
 end
